8139c0bff9e82df6e025f5fdfe9da4825f0fead8
Juan Araujo <juan110588@gmail.com>
2024-09-21 11:49:43 -0300

    Adicionando calendario

--- app/src/main/java/com/example/recoope_mobile/activities/company/CalendarFragment.java
+++ app/src/main/java/com/example/recoope_mobile/activities/company/CalendarFragment.java
@@ -1,8 +1,10 @@
 package com.example.recoope_mobile.activities.company;
 
 import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.Gravity;
@@ -15,13 +17,16 @@ import android.widget.TextView;
 import com.example.recoope_mobile.R;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 public class CalendarFragment extends Fragment {
+    private String todayDayOfWeek, todayMonth;
+    private int todayDay, todayYear;
     private GridLayout gridLayout;
-    private TextView dateTextView, monthTextView, yearTextView;
+    private TextView monthTextView, yearTextView;
     private Calendar calendar;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "DefaultLocale"})
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,6 +36,15 @@ public class CalendarFragment extends Fragment {
         monthTextView = view.findViewById(R.id.textMonth);
         yearTextView = view.findViewById(R.id.textYear);
         calendar = Calendar.getInstance();
+
+        todayDayOfWeek = getDayOfWeekPtBr();
+        todayDay = calendar.get(Calendar.DAY_OF_MONTH);
+        todayMonth = getMonthPtBr().toLowerCase();
+        todayYear = getYear();
+
+        ((TextView) view.findViewById(R.id.textDate)).setText(
+                String.format("%s, %d de %s", todayDayOfWeek, todayDay, todayMonth)
+        );
 
         updateCalendar();
 
@@ -54,15 +68,16 @@ public class CalendarFragment extends Fragment {
         int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
         int firstDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1;
 
-        String monthName = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, getResources().getConfiguration().locale);
-        int year = calendar.get(Calendar.YEAR);
-        monthTextView.setText(Translate.fromMonth(monthName));
+        String monthName = getMonthPtBr();
+        int year = getYear();
+        monthTextView.setText(monthName);
         yearTextView.setText(String.valueOf(year));
 
         for (int i = 0; i < firstDayOfWeek; i++) addDayToGridLayout(0);
         for (int day = 1; day <= daysInMonth; day++) addDayToGridLayout(day);
     }
 
+    @SuppressLint("ResourceAsColor")
     private void addDayToGridLayout(int day) {
         String dayText = day != 0 ? String.valueOf(day) : "";
 
@@ -70,6 +85,14 @@ public class CalendarFragment extends Fragment {
         dayTextView.setText(dayText);
         dayTextView.setGravity(Gravity.CENTER);
         dayTextView.setTextSize(16f);
+        dayTextView.setTypeface(null, Typeface.BOLD);
+
+        // Mudando cor do dia atual.
+        if (day == todayDay &&
+            getMonthPtBr().equalsIgnoreCase(todayMonth) &&
+            getYear() == todayYear) {
+            dayTextView.setTextColor(ContextCompat.getColor(getContext(), R.color.recoope_light_blue_color));
+        }
 
         GridLayout.LayoutParams params = new GridLayout.LayoutParams();
         params.width = 0;
@@ -82,7 +105,28 @@ public class CalendarFragment extends Fragment {
         gridLayout.addView(dayTextView);
     }
 
-    public enum Translate {
+    private String getDayOfWeekPtBr() {
+        String dow = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US);
+        return Translate.from(dow);
+    }
+
+    private String getMonthPtBr() {
+        String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US);
+        return Translate.from(month);
+    }
+
+    private int getYear() {
+        return calendar.get(Calendar.YEAR);
+    }
+
+    private enum Translate {
+        SUNDAY("Dom"),
+        MONDAY("Dom"),
+        TUESDAY("Dom"),
+        WEDNESDAY("Dom"),
+        THURSDAY("Dom"),
+        FRIDAY("Sex"),
+        SATURDAY("Sab"),
         JANUARY("Janeiro"),
         FEBRUARY("Fevereiro"),
         MARCH("Março"),
@@ -96,18 +140,18 @@ public class CalendarFragment extends Fragment {
         NOVEMBER("Novembro"),
         DECEMBER("Dezembro");
 
-        private final String month;
-        Translate(String month) { this.month = month; }
+        private final String translation;
+        Translate(String translation) { this.translation = translation; }
 
-        public String getMonth() {
-            return month;
+        public String getTranslation() {
+            return translation;
         }
 
-        public static String fromMonth(String month) {
+        public static String from(String month) {
 
             for (Translate respost : Translate.values()) {
                 if (respost.toString().equalsIgnoreCase(month)) {
-                    return respost.getMonth();
+                    return respost.getTranslation();
                 }
             }
             throw new IllegalArgumentException("Unrecognized response month: " + month);
