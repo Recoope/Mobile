88f65830762e0a51b17027d13886d3a49cd4400b
Juan Araujo <juan.araujo@picpay.com>
2024-10-09 18:45:23 -0300

    tela de lance

--- app/src/main/java/com/example/recoope_mobile/activity/fragments/MainActivity.java
+++ app/src/main/java/com/example/recoope_mobile/activity/fragments/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     private void replaceFragment(Fragment fragment)  {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.companyContent, fragment);
+        fragmentTransaction.replace(R.id.mainContent, fragment);
         fragmentTransaction.commit();
     }
 
